339659f05587a96b82e72511e32058b04f140b83
Andy Botting <andy@andybotting.com>
2016-02-17 11:12:54 +1100

    Fix dashboard param name
    Change-Id: I1d7ba89f5153e6d253ab6c101cc9e97f80ce88f4

--- manifests/settings/dashboard.pp
+++ manifests/settings/dashboard.pp
@@ -4,17 +4,17 @@
 #    (Optional) Whether or not Horizon is expected to be available
 #    Defaults to $::os_service_default
 #
-#  [*dashboard_dashboard_url*]
+#  [*url*]
 #    Defaults to $::os_service_default
 #
 class tempest::settings::dashboard (
   $service_available       = $::os_service_default,
-  $dashboard_dashboard_url = $::os_service_default,
+  $url                     = $::os_service_default,
 ) {
 
   tempest_config {
     'service_available/horizon': value => $service_available;
-    'dashboard/dashboard_url':   value => $dashboard_dashboard_url;
+    'dashboard/dashboard_url':   value => $url;
   }
 
 }
