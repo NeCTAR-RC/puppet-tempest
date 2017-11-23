0550542a69157d71a664ffc266119a4e063b72b6
Sam Morrison <sorrison@gmail.com>
2017-11-23 16:40:43 +1100

    Use python3 for virtualenv
    Change-Id: I1bd074822cd730baaad9e93e9aef53df0ccf22aa

--- manifests/install.pp
+++ manifests/install.pp
@@ -46,7 +46,7 @@ class tempest::install (
     if $setup_venv {
 
       exec { 'setup-venv':
-        command => "virtualenv ${venv_path}",
+        command => "virtualenv -p python3 ${venv_path}",
         path    => '/usr/bin',
         creates => "${venv_path}/bin/activate",
       }
