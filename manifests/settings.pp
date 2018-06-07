ac721799a7dd2224aa6db60436b65cac77e54e07
Shi Yan <shi.yan@unimelb.edu.au>
2018-06-07 16:59:24 +1000

    Remove deprecated code and rename deprecated options
    Change-Id: I74711718cbe1b9a18d74adb2ee7101ab124b804d

--- manifests/settings.pp
+++ manifests/settings.pp
@@ -105,7 +105,7 @@ class tempest::settings (
   $oslo_concurrency_lock_path                = $::os_service_default,
   $oslo_concurrency_disable_process_locking  = $::os_service_default,
   # DEPRECATED PARAMETERS
-  $admin_tenant_name                         = $::os_service_default,
+  $admin_tenant_name                         = undef,
 ) {
 
   include ::tempest::settings::alarming
@@ -128,10 +128,10 @@ class tempest::settings (
 
   if $admin_tenant_name {
     warning('The tempest::config::admin_tenant_name parameter is deprecated, use tempest::config::admin_project_name instead.')
-    $admin_project_name_real = $tenant_name
+    $admin_project_name_real = $admin_tenant_name
   }
   else {
-    $admin_project_name_real = $project_name
+    $admin_project_name_real = $admin_project_name
   }
 
   if $test_accounts {
@@ -159,7 +159,6 @@ class tempest::settings (
     'DEFAULT/resources_prefix':                 value => $resources_prefix;
     'auth/test_accounts_file':                  value => $_test_accounts_file;
     'auth/use_dynamic_credentials':             value => $use_dynamic_credentials;
-    'auth/tempest_roles':                       value => $tempest_roles;
     'auth/default_credentials_domain_name':     value => $default_credentials_domain_name;
     'auth/create_isolated_networks':            value => $create_isolated_networks;
     'auth/admin_username':                      value => $admin_username;
@@ -169,5 +168,6 @@ class tempest::settings (
     'oslo_concurrency/lock_path':               value => $oslo_concurrency_lock_path;
     'oslo_concurrency/disable_process_locking': value => $oslo_concurrency_disable_process_locking;
   }
+
 
 }
